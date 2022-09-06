559b34f6015c9941be98755be799611edb88bf14
Bennet Robin Fabian <bennet@bennet.dev>
2022-09-06 17:27:35 +0200

    Added Min Sans

---
+++ Casks/font-min-sans.rb
@@ -0,0 +1,20 @@
+cask "font-min-sans" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/poposnail61/min-sans/trunk/fonts/static",
+      using: :svn
+  name "Min Sans"
+  desc "Min Sans font"
+  homepage "https://github.com/poposnail61/min-sans"
+
+  font "MinSans-Thin.otf"
+  font "MinSans-Black.otf"
+  font "MinSans-Bold.otf"
+  font "MinSans-ExtraBold.otf"
+  font "MinSans-ExtraLight.otf"
+  font "MinSans-Light.otf"
+  font "MinSans-Medium.otf"
+  font "MinSans-Regular.otf"
+  font "MinSans-SemiBold.otf"
+end
