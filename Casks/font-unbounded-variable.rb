eb5988ec985797e224beabcd610022b95bde97af
Bennet Robin Fabian <bennet@bennet.dev>
2022-09-11 18:53:02 +0200

    Added Unbounded (variable & static)

---
+++ Casks/font-unbounded-variable.rb
@@ -0,0 +1,13 @@
+cask "font-unbounded-variable" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/w3f/unbounded/trunk/Variable",
+      using:    :svn,
+      verified: "https://github.com/w3f/unbounded"
+  name "Unbounded"
+  desc "Unbounded font"
+  homepage "https://unbounded.polkadot.network/"
+
+  font "Unbounded-Variable.ttf"
+end
---
+++ Casks/font-unbounded.rb
@@ -0,0 +1,18 @@
+cask "font-unbounded" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/w3f/unbounded/trunk/TTF",
+      using:    :svn,
+      verified: "https://github.com/w3f/unbounded"
+  name "Unbounded"
+  desc "Unbounded font"
+  homepage "https://unbounded.polkadot.network/"
+
+  font "Unbounded-Black.ttf"
+  font "Unbounded-Blond.ttf"
+  font "Unbounded-Bold.ttf"
+  font "Unbounded-Light.ttf"
+  font "Unbounded-Medium.ttf"
+  font "Unbounded-Regular.ttf"
+end
