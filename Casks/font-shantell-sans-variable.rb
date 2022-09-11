d70bcdba106bcd3c1bcdf9f1934aff1d7164c435
Bennet Robin Fabian <bennet@bennet.dev>
2022-09-11 18:47:36 +0200

    Added Shantell Sans (variable & static)

---
+++ Casks/font-shantell-sans-variable.rb
@@ -0,0 +1,12 @@
+cask "font-shantell-sans-variable" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/arrowtype/shantell-sans/trunk/fonts/Shantell%20Sans/Desktop",
+      using: :svn
+  name "Shantell Sans"
+  desc "Shantell Sans font"
+  homepage "https://github.com/arrowtype/shantell-sans"
+
+  font "ShantellSans[BNCE,IRGL,TRAK,ital,wght].ttf"
+end
