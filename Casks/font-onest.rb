38e79fcd6a1eae4291492a0a133e9d8a203d42f4
Bennet Robin Fabian <bennet@bennet.dev>
2023-04-18 16:27:43 +0200

    Added Onest

---
+++ Casks/font-onest.rb
@@ -0,0 +1,17 @@
+cask "font-onest" do
+  version :latest
+  sha256 :no_check
+
+  url "https://raw.githubusercontent.com/vermoh/Onest/main/Onest.zip", verified: "raw.githubusercontent.com/vermoh/Onest/main/Onest.zip"
+  name "font-onest"
+  desc "The first typeface designed for Moldova to make communication more open and honest."
+  homepage "https://onest.md/en"
+
+  font "TTF/Onest-Black.ttf"
+  font "TTF/Onest-Bold.ttf"
+  font "TTF/Onest-ExtraBold.ttf"
+  font "TTF/Onest-Light.ttf"
+  font "TTF/Onest-Medium.ttf"
+  font "TTF/Onest-Regular.ttf"
+  font "TTF/Onest-Thin.ttf"
+end
